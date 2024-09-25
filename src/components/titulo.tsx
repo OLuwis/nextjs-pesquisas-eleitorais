c453066a9cb3890eae4d68e0d837e23db843abf2
Luis Miguel <oluismrs@gmail.com>
2024-09-24 23:33:48 -0300

    feat: criar componente titulo

---
+++ src/components/titulo.tsx
@@ -0,0 +1,9 @@
+export default function Titulo() {
+  return (
+    <header>
+      <div className="font-black text-4xl text-center py-6">
+        <span>Pesquisas Eleitorais <span className="bg-gradient-to-r from-blue-400 to-red-400 text-transparent bg-clip-text">2024</span></span>
+      </div>
+    </header>
+  )
+}
