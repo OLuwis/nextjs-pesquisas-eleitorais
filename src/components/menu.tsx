cd0e09714fe715c30f62e9a8d930f4c2b2722e6c
Luis Miguel <oluismrs@gmail.com>
2024-09-27 23:29:56 -0300

    feat: criar menu usando componentes do shadcn

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,10 +1,31 @@
-import Seletor from "./seletor";
+import fontes from "@/commons/fontes.json";
+import Seletor from "@/components/seletor";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
+import { Label } from "./ui/label";
+import { Button } from "./ui/button";
 
 export default function Menu() {
   return (
-    <nav className="border border-gray-500 rounded-md m-auto w-fit p-4 flex flex-col gap-3">
-      <Seletor placeholder="Selecione uma capital" opcoes={["Opção 1", "Opção 2"]} />
-      <Seletor placeholder="Selecione um instituto" opcoes={["Datafolha", "Quaest"]} />
-    </nav>
+    <Card className="w-max m-auto">
+      <CardHeader>
+        <CardTitle>Parciais - 1º Turno</CardTitle>
+        <CardDescription>Visualize as parciais do 1º turno das eleições de 2024</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="grid w-full items-center gap-4">
+          <div className="flex flex-col space-y-1.5">
+            <Label>Capitais</Label>
+            <Seletor placeholder="Selecione uma capital" opcoes={fontes.map(f => f.nome)} />
+          </div>
+          <div className="flex flex-col space-y-1.5">
+            <Label>Institutos</Label>
+            <Seletor placeholder="Selecione um instituto" opcoes={["Datafolha", "Quaest"]} />
+          </div>
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Button className="w-full">Ver Resultados</Button>
+      </CardFooter>
+    </Card>
   )
 }
