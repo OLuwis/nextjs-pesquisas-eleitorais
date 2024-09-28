26464d5acdfef5b7c9ed290ab8bb7afa98c85b64
Luis Miguel <oluismrs@gmail.com>
2024-09-27 23:31:55 -0300

    chore: refazer o titulo usando o ativador de tema escuro e claro

--- src/components/titulo.tsx
+++ src/components/titulo.tsx
@@ -1,9 +1,12 @@
+import AtivadorTema from "./ativadorTema"
+
 export default function Titulo() {
   return (
-    <header>
-      <div className="font-black text-4xl text-center py-6">
-        <span>Pesquisas Eleitorais <span className="bg-gradient-to-r from-blue-400 to-red-400 text-transparent bg-clip-text">2024</span></span>
-      </div>
-    </header>
+    <div className="flex items-center justify-center py-6 opacity-90">
+      <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0 mr-2.5">
+        Pesquisas Eleitorais <span className="bg-gradient-to-r from-blue-500 to-red-500 dark:from-blue-400 dark:to-red-400 text-transparent bg-clip-text">2024</span>
+      </h2>
+      <AtivadorTema />
+    </div>
   )
 }
