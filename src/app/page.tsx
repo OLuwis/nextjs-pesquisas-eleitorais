1384c208cb4af62b8855030dfe74030a987aa094
Luis Miguel <oluismrs@gmail.com>
2024-10-01 04:00:02 -0300

    chore: remover linhas inutilizaveis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,12 @@
-import { Capital } from "@/types/capital";
-import dados from "@/commons/fontes.json"
 import Menu from "@/components/menu";
 import Titulo from "@/components/titulo";
 import Grafico from "@/components/grafico";
 
-export default async function Home() {
-  // const capitais: Capital[] = dados as Capital[]
-  // const salvador: Capital | undefined = capitais.find(c => c.nome === "Salvador")
-  // const link = salvador?.links.quaest || ""
-
-  // const res = await fetch(link)
-  // const data = res.text()
-
+export default function Home() {
   return (
     <div className="max-w-screen-xl m-auto">
       <Titulo />
       <Menu />
-      <Grafico />
     </div>
   );
 }
