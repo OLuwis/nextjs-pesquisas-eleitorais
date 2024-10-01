5093db8a6a3e3f0eb3a6b8cc24227a99e27a21fe
Luis Miguel <oluismrs@gmail.com>
2024-10-01 03:58:09 -0300

    feat: adicionar tooltip customizada, customizar XAxis & remover amostras

--- src/components/grafico.tsx
+++ src/components/grafico.tsx
@@ -1,94 +1,74 @@
 "use client"
 
-import amostra from "@/commons/amostra.json";
-import { Pesquisa } from "@/types/pesquisa";
-import { ChartConfig, ChartContainer } from "@/components/ui/chart";
-import { ChartTooltip, ChartTooltipContent } from "@/components/ui/chart";
-import { ChartLegend, ChartLegendContent } from "@/components/ui/chart";
-import { Line, LineChart } from "recharts";
-import { CartesianGrid, XAxis } from "recharts";
-// import { ChartData, ChartDataset, Point, Chart, registerables, ScaleChartOptions } from "chart.js";
-// import { Line } from "react-chartjs-2";
-// import "chartjs-adapter-date-fns";
+import { Pesquisa } from "@/types/pesquisa"
+import { criarConfig, criarDados } from "@/services/graficoService"
+import { ChartContainer, ChartTooltip, ChartTooltipContent, ChartLegend, ChartLegendContent } from "@/components/ui/chart";
+import { Line, LineChart, CartesianGrid, XAxis } from "recharts";
+import { CSSProperties } from "react";
 
-export default function Grafico() {
-  const chartData = [
-    { month: "January", desktop: 186, mobile: 80 },
-    { month: "February", desktop: 305, mobile: 200 },
-    { month: "March", desktop: 237, mobile: 120 },
-    { month: "April", desktop: 73, mobile: 190 },
-    { month: "May", desktop: 209, mobile: 130 },
-    { month: "June", desktop: 214, mobile: 140 },
-  ]
-
-  const chartConfig = {
-    desktop: {
-      label: "Desktop",
-      color: "#2563eb",
-    },
-    mobile: {
-      label: "Mobile",
-      color: "#60a5fa",
-    },
-  } satisfies ChartConfig
+export default function Grafico({ pesquisa }: { pesquisa: Pesquisa }) {
+  const dados = criarDados(pesquisa)
+  const config = criarConfig(pesquisa)
 
   return (
-    <ChartContainer config={chartConfig} className="max-h-[324px] w-full">
-      <LineChart accessibilityLayer data={chartData} margin={{ left: 12, right: 12 }}>
+    <ChartContainer config={config} className="max-h-[324px] w-full mt-6">
+      <LineChart accessibilityLayer data={dados} margin={{ left: 18, right: 18 }}>
         <CartesianGrid vertical={false} />
         <XAxis
-          dataKey="month"
-          tickLine={false}
+          dataKey="date"
           tickMargin={10}
+          tickLine={false}
           axisLine={false}
-          tickFormatter={(value) => value.slice(0, 3)}
+          tickFormatter={(v) => {
+            return new Date(v).toLocaleDateString("pt-BR", {
+              month: "numeric",
+              day: "numeric"
+            })
+          }}
         />
-        <ChartTooltip content={<ChartTooltipContent />} />
+        <ChartTooltip content={<ChartTooltipContent
+          formatter={(value, name) => (
+            <>
+              <div
+                className="h-2.5 w-2.5 shrink-0 rounded-[2px] bg-[--color-bg] mt-1"
+                style={
+                  {
+                    "--color-bg": `var(--color-${name})`
+                  } as CSSProperties
+                }
+              />
+              {config[name as keyof typeof config]?.label || name}
+              <div className="ml-auto flex items-baseline font-mono font-medium tabular-nums text-foreground">
+                {value}
+                <span className="font-normal text-muted-foreground">
+                  %
+                </span>
+              </div>
+            </>
+          )}
+          labelFormatter={(value) => {
+            return "1º Turno - " + new Date(value).toLocaleDateString("pt-BR", {
+              month: "numeric",
+              day: "numeric",
+              year: "numeric"
+            })
+          }}
+          indicator="line"
+        />} />
         <ChartLegend content={<ChartLegendContent />} />
-        <Line dataKey="desktop" type="natural" stroke="var(--color-desktop)" strokeWidth={2} dot={false} />
-        <Line dataKey="mobile" type="natural" stroke="var(--color-mobile)" strokeWidth={2} dot={false} />
+        {pesquisa.resultado.cenarios[0].data.map(d => (
+          <Line
+            dataKey={d.option.replace(/[^a-zA-Z0-9]/g, "")}
+            type="linear"
+            stroke={`var(--color-${d.option.replace(/[^a-zA-Z0-9]/g, "")})`}
+            strokeWidth={2}
+            dot={{
+              fill: `var(--color-${d.option.replace(/[^a-zA-Z0-9]/g, "")})`,
+              r: 2
+            }}
+            key={d.option.replace(/[^a-zA-Z0-9]/g, "")} />
+        ))}
       </LineChart>
     </ChartContainer>
   )
 }
-
-// Chart.register(...registerables)
-
-// export default function Grafico() {
-//   const dados = amostra.resultado.cenarios[0].data
-
-//   const cores = dados.map(d => d.color)
-//   const opcoes = dados.map(d => d.option)
-//   const valoresIniciais = dados.map(d => d.values[0].value)
-//   const valoresFinais = dados.map(d => d.values[1].value)
-//   const datasIniciais = dados.map(d => d.values[0].date)
-//   const datasFinais = dados.map(d => d.values[1].date)
-
-//   const _dados: ChartDataset<"line", (number | Point | null)[]>[] = []
-
-//   dados.forEach(d => _dados.push(
-//     {
-//       label: d.option,
-//       data: [d.values[0].value * 100, d.values[1].value * 100],
-//       backgroundColor: d.color,
-//       borderColor: d.color
-//     }
-//   ))
-
-//   const dataset: ChartData<"line", (number | Point | null)[], unknown> = {
-//     labels: ["2024-08-27T00:00:00Z", "2024-09-17T00:00:00Z"],
-//     datasets: _dados
-//   }
-
-//   return (
-//     <div>
-//       <Line data={dataset} options={{ interaction: { intersect: false, mode: "index" }, plugins: { title: { text: amostra.resultado.cenarios[0].bandeira, display: true }, tooltip: { callbacks: { title: ctx => "Datafolha - " + ctx[0].label, label: ctx => ctx.dataset.label + ": " + ctx.raw + "%" } } }, scales: { x: { type: "time", time: { unit: "day", displayFormats: { day: "dd/MM" }, tooltipFormat: "dd/MM/yyyy" }, ticks: { source: "labels" } } } }} />
-//       {/* <p className="my-4">{"Cores: " + JSON.stringify(cores)}</p>
-//       <p className="my-4">{"Opcoes: " + JSON.stringify(opcoes)}</p>
-//       <p className="my-4">{"Datas Iniciais: " + JSON.stringify(datasIniciais)}</p>
-//       <p className="my-4">{"Datas Finais: " + JSON.stringify(datasFinais)}</p>
-//       <p className="my-4">{"Valores Iniciais: " + JSON.stringify(valoresIniciais)}</p>
-//       <p className="my-4">{"Valores Finais: " + JSON.stringify(valoresFinais)}</p> */}
-//     </div>
-//   )
-// }
