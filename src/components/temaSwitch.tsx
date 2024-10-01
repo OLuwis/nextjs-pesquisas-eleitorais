305aac555387ba5a38591a3c754ccc314d4f1dea
Luis Miguel <oluismrs@gmail.com>
2024-10-01 03:54:44 -0300

    chore: renomear componentes de Tema

--- src/components/temaSwitch.tsx
+++ src/components/temaSwitch.tsx
@@ -5,15 +5,25 @@ import { MoonIcon, SunIcon } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { useTheme } from "next-themes"
 
-export default function AtivadorTema() {
+export default function TemaSwitch() {
   const { theme, setTheme } = useTheme()
 
   return theme === "light" ? (
-    <Button variant="outline" size="icon" className="rounded-full" onClick={() => setTheme("dark")}>
+    <Button
+      variant="outline"
+      size="icon"
+      className="rounded-full"
+      onClick={() => setTheme("dark")}
+    >
       <MoonIcon className="h-6 w-6" />
     </Button>
   ) : (
-    <Button variant="outline" size="icon" className="rounded-full" onClick={() => setTheme("light")}>
+    <Button
+      variant="outline"
+      size="icon"
+      className="rounded-full"
+      onClick={() => setTheme("light")}
+    >
       <SunIcon className="h-6 w-6" />
     </Button>
   )
