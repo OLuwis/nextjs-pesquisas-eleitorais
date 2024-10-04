6a53ebc740491c6b5f0f548512e0bba485039496
Luis Miguel <oluismrs@gmail.com>
2024-10-04 00:29:24 -0300

    chore: mudar de título para cabeçalho

--- src/components/cabecalho.tsx
+++ src/components/cabecalho.tsx
@@ -1,15 +1,15 @@
 import TemaSwitch from "@/components/temaSwitch"
 import Link from "next/link"
 
-export default function Titulo() {
+export default function Cabecalho() {
   return (
-    <div className="flex items-center justify-center py-6 opacity-90">
+    <header className="flex items-center justify-center py-5 opacity-90">
       <Link href="/">
         <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0 mr-2.5">
           Pesquisas Eleitorais <span className="bg-gradient-to-r from-blue-500 to-red-500 dark:from-blue-400 dark:to-red-400 text-transparent bg-clip-text">2024</span>
         </h2>
       </Link>
       <TemaSwitch />
-    </div>
+    </header>
   )
 }
